a4d6f2f0acd52f02c52fa2037ba2add29c7d2dac
Nikolas Praschma <nick.praschma@tryjungle.com>
2022-12-03 15:44:57 -0500

    Added directional arrow functionality to TopBar

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css'
 import UserForm from './components/UserForm';
 import Stock from './components/Stock';
 import TopBar from './components/TopBar';
 
-const fakeStocks = [ 
-    {
-    "symbol" : "MSFT",
-    "companyName" : "Microsoft Corporation",
-    "marketCap" : 1391637040000,
-    "sector" : "Technology",
-    "beta" : 1.2310280000000000111270992420031689107418060302734375,
-    "price" : 183.509999999999990905052982270717620849609375,
-    "lastAnnualDividend" : 1.939999999999999946709294817992486059665679931640625,
-    "volume" : 54536583,
-    "exchange" : "Nasdaq Global Select",
-    "exchangeShortName" : "NASDAQ"
-    },
-    {
-        "symbol" : "AAPL",
-        "companyName" : "Apple Inc.",
-        "marketCap" : 1382174560000,
-        "sector" : "Technology",
-        "beta" : 1.2284990000000000076596506914938800036907196044921875,
-        "price" : 318.8899999999999863575794734060764312744140625,
-        "lastAnnualDividend" : 3.0800000000000000710542735760100185871124267578125,
-        "volume" : 51500795,
-        "exchange" : "Nasdaq Global Select",
-        "exchangeShortName" : "NASDAQ"
-    }, 
-    {
-        "symbol" : "AMZN",
-        "companyName" : "Amazon.com Inc.",
-        "marketCap" : 1215457260000,
-        "sector" : "Technology",
-        "beta" : 1.5168630000000000723758830645238049328327178955078125,
-        "price" : 2436.8800000000001091393642127513885498046875,
-        "lastAnnualDividend" : 0,
-        "volume" : 6105985,
-        "exchange" : "Nasdaq Global Select",
-        "exchangeShortName" : "NASDAQ"
-    }
-]
-
 const App = () => {
     const [stocks, setStocks] = useState([]);
     const [sortingMethod, setSortingMethod] = useState("quicksort");
-
-    // useEffect(() => {
-    //     console.log("Stocks changed");
-    // }, [stocks])
+    const [resetFlag, setResetFlag] = useState(false);
 
     const searchStocks = async (filters) => {
-        console.log(filters);
+        console.log("filters: ", filters);
         const response = await fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=${filters.marketCapMoreThan}&marketCapLowerThan=${filters.marketCapLowerThan}&betaMoreThan=${filters.betaMoreThan}&betaLowerThan=${filters.betaLowerThan}&volumeMoreThan=${filters.volumeMoreThan}&volumeLowerThan=${filters.volumeLowerThan}&priceMoreThan=${filters.priceMoreThan}&priceLowerThan=${filters.priceLowerThan}&exchange=NASDAQ&limit=${filters.limit}&apikey=a5df1e34a66d2eeb25448eb9a1f2655f`);
         const data = await response.json();
         setStocks(data);
-        console.log((data));
+        console.log("stock data: ", data);
+
+        // Reset sorting parameters when search button is clicked
+        setResetFlag(resetFlag => !resetFlag);
     }
 
     // Sorts stocks alphabetically by symbol using built-in sorting algorithm
-    const sortStocks = () => {
+    const sortStocks = (sortBy, sortAscending) => {
         let currentStocks = [...stocks];
-        currentStocks.sort((a, b) => a.symbol.localeCompare(b.symbol));
+
+        /*
+        Use the following three parameters to sort the stocks using custom sorting algorithm
+        1. sortingMethod (either "timsort" or "quicksort"), 
+        2. sortBy (either "symbol", "price", "marketCap", "volume", or "beta"),
+        3. sortAscending (either true or false)
+        */
+        console.log("Inside of sortStocks(), sortingMethod is", sortingMethod);
+        console.log("Inside of sortStocks(), sortBy is ", sortBy);
+        console.log("Inside of sortStocks(), sortAscending is ", sortAscending);
+        if (sortAscending) {
+            currentStocks.sort((a, b) => a.price - b.price);
+        }
+        else {
+            currentStocks.sort((a, b) => b.price - a.price);
+        }
         setStocks(currentStocks);
     }
 
@@ -83,9 +59,8 @@ const App = () => {
                     onClick={() => {setSortingMethod("timsort")}}>
                 Timsort
                 </button>  
-                <button onClick={sortStocks}>Sort stocks</button>
                 <div className="grid">
-                    <TopBar />
+                    <TopBar setParameters={sortStocks} reset={resetFlag}/>
                     {stocks.map((stock, index) => (
                         index < 500 && <Stock stock={stock}/>
                     ))}
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,59 @@
-const TopBar = () => {
+import { useEffect, useState } from "react";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
+
+const TopBar = ({ setParameters, reset }) => {
+    const [sortBy, setSortBy] = useState("");
+    const [sortAscending, setSortAscending] = useState(false);
+
+    const sendData = (newSortBy) => {
+        let newSortAscending = !sortAscending;
+        setSortBy(newSortBy);
+        setSortAscending(sortAscending => !sortAscending);
+        setParameters(newSortBy, newSortAscending);
+    }
+
+    useEffect(() => {
+        setSortBy("");
+        setSortAscending(false);
+    }, [reset])
+
     return (
         <div>
-            <button className="topBarButton">Ticker</button>
-            <button className="topBarButton">Price</button>
-            <button className="topBarButton">Market Cap</button>
-            <button className="topBarButton">Volume</button>
-            <button className="topBarButton">Beta</button>
+            <button className="topBarButton" onClick={() => sendData("symbol")}>
+                {(sortBy === "symbol" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "symbol" && !sortAscending) && <MdKeyboardArrowDown />}
+                <span> Ticker </span>
+                {(sortBy === "symbol" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "symbol" && !sortAscending) && <MdKeyboardArrowDown />}
+            </button>
+            <button className="topBarButton" onClick={() => sendData("price")}>
+                {(sortBy === "price" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "price" && !sortAscending) && <MdKeyboardArrowDown />}
+                <span> Price </span>
+                {(sortBy === "price" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "price" && !sortAscending) && <MdKeyboardArrowDown />}
+            </button>
+            <button className="topBarButton" onClick={() => sendData("marketCap")}>
+                {(sortBy === "marketCap" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "marketCap" && !sortAscending) && <MdKeyboardArrowDown />}
+                <span> Market Cap </span>
+                {(sortBy === "marketCap" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "marketCap" && !sortAscending) && <MdKeyboardArrowDown />}
+            </button>
+            <button className="topBarButton" onClick={() => sendData("volume")}>
+                {(sortBy === "volume" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "volume" && !sortAscending) && <MdKeyboardArrowDown />}
+                <span> Volume </span>
+                {(sortBy === "volume" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "volume" && !sortAscending) && <MdKeyboardArrowDown />}
+            </button>
+            <button className="topBarButton" onClick={() => sendData("beta")}>
+                {(sortBy === "beta" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "beta" && !sortAscending) && <MdKeyboardArrowDown />}
+                <span> Beta </span>
+                {(sortBy === "beta" && sortAscending) && <MdKeyboardArrowUp />}
+                {(sortBy === "beta" && !sortAscending) && <MdKeyboardArrowDown />}
+            </button>
         </div>
     )
 }
